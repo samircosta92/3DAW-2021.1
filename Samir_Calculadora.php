d4b21204442428716b27c314534454e40a37eb48
Samir Costa <65731013+samircosta92@users.noreply.github.com>
2021-04-13 10:06:03 -0300

    V2.0 da Calculadora
    Adicionadas as operações %, 1/x, x^2 e sqrt(), além da verificação para divisões por 0.

--- Samir_Calculadora.php
+++ Samir_Calculadora.php
@@ -35,16 +35,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     </style>
 </head>
 <body>
-<form action="Samir_Calculadora.php" method="post">
+<form action="Samir_Calculadora_V2.0.php" method="post">
     <fieldset name="calculadora">
         <legend>Calculadora:</legend><br>
         Primeiro valor:<input type="text" name="num1"><br><br>
         Segundo valor:<input type=text" name="num2"><br><br>
-        Operação: <br><input type="radio" name="op" value="soma" checked>Soma<br>
-        <input type="radio" name="op" value="subt">Subtração<br>
-        <input type="radio" name="op" value="mult">Multiplicação<br>
-        <input type="radio" name="op" value="div">Divisão<br>
-        <input type="radio" name="op" value="pot">Potência<br>
+        Operação: <br><input type="radio" name="op" value="soma" checked>Soma (+)
+        <input type="radio" name="op" value="subt">Subtração (-)<br>
+        <input type="radio" name="op" value="mult">Multiplicação (x)
+        <input type="radio" name="op" value="div">Divisão (/)<br>
+        <input type="radio" name="op" value="pot">Potência
+        <input type="radio" name="op" value="perc">Porcentagem (%)<br>
+        <input type="radio" name="op" value="inverso">1/X (Para o primeiro valor)<br>
+        <input type="radio" name="op" value="quadrado">X² (Para o primeiro valor)<br>
+        <input type="radio" name="op" value="raiz">√X (Para o primeiro valor)<br>
         <input type="submit" value="ENVIAR">
     </fieldset>
 </form>
@@ -52,7 +56,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
+    if(($op == "inverso" or $op == "quadrado" or $op == "raiz") and $num1Valido == 1)
+    {
+        if($op == "inverso")
+        {
 
+            if($num1 == 0)
+            {
+                echo "<h2>Não é possível dividir por 0!</h2>";
+            }
+            else {
+                $resp = 1/$num1;
+                echo "<br>";
+                echo "<h2>1/$num1 = $resp</h2>";
+            }
+        }
+        if($op == "quadrado")
+        {
+            $resp = $num1*$num1;
+            echo "<br>";
+            echo "<h2>$num1 ² = $resp</h2>";
+
+        }
+        if($op == "raiz")
+        {
+            $resp = sqrt($num1);
+            echo "<br>";
+            echo "<h2>√$num1 = $resp</h2>";
+
+        }
+    }
+    else
     if ($num1Valido == 1 and $num2Valido == 1)
     {
         if($op == "soma")
@@ -78,9 +112,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
         if($op == "div")
         {
-            $resp = $num1/$num2;
-            echo "<br>";
-            echo "<h2>$num1 / $num2 = $resp</h2>";
+            if ($num2 == 0) {
+                echo "<h2>Não é possível dividir por 0!</h2>";
+            } else {
+                $resp = $num1 / $num2;
+                echo "<br>";
+                echo "<h2>$num1 / $num2 = $resp</h2>";
+            }
         }
 
         if($op == "pot")
@@ -90,8 +128,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             echo "<h2>$num1 ^ $num2 = $resp</h2>";
 
         }
+        if($op == "perc")
+        {
+            $resp = $num1*($num2/100);
+            echo "<br>";
+            echo "<h2>$num2% de $num1 = $resp</h2>";
+
+        }
+
+
     }
-    else {
+    else
+        {
         if ($num1Valido == 0 and $num2Valido == 1) {
             echo "<h2>Digite o primeiro valor corretamente!</h2>";
         }
