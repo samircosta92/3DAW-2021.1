e0532300642c26779d27bd6f16fc209b2c6a703e
Samir Costa <65731013+samircosta92@users.noreply.github.com>
2021-06-15 17:45:10 -0300

    Atualizando o exercício com conexão ao banco
    - Campo select com os estados pré determinados
    - Conexão com o banco mysql para buscar as cidades
    - Correção para os caracteres especiais

--- Ajax_Json_Select/ex22AjaxJSON.php
+++ Ajax_Json_Select/ex22AjaxJSON.php
@@ -1,7 +1,42 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $nome = $_GET["estado"];
-    echo '{"cidades":["Rio de Janeiro", "Angra", "Caxias"]}';
+
+$estado = $_GET["estado"];
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$nomeBanco = "dawfaeterj";
+
+$conn = new mysqli($servidor, $usuario, $senha, $nomeBanco);
+
+$query = "SELECT * FROM `estado` WHERE `uf`='$estado'";
+$result = $conn->query($query);
+$linha = $result->fetch_assoc();
+$idEstado = $linha["id"];
+
+$query2 = "SELECT * FROM `cidade` WHERE `estado`='$idEstado'";
+$result2 = $conn->query($query2);
+$linha2 = $result2->fetch_assoc();
+$nomeCidade = $linha2["nome"];
+
+
+$dados = array();
+$i=0;
+
+do{
+    $dados[$i] = $linha2["nome"];
+    $i++;
+}while($linha2 = $result2->fetch_assoc());
+
+//função para consertar os caracteres especiais
+    function jsonEncode($dados){
+        array_walk_recursive( $dados, function(&$item) {
+            $item = utf8_encode( $item );
+        });
+        return json_encode($dados);
+    }
+
+echo json_encode($dados);
 
 }
 ?>
